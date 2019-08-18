624c988e004ed6316b9cf5cc12608a435382cb83
vipin-2101 <vipin_2101@yahoo.com>
2019-08-18 14:28:20 -0500

    changed

--- bin/run-tests-in-virtual-screen.sh
+++ bin/run-tests-in-virtual-screen.sh
@@ -7,7 +7,7 @@ then
         --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" \
         robot \
         --outputDir /app/reports \
-        -e BROWSER=chrome --shm-size=1g \
+        -e BROWSER=chrome \
         /app/test
 else
     xvfb-run \
