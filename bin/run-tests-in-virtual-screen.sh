0b31b40cb4b1b3f608c6b9353b169d5086be223b
vipin-2101 <vipin_2101@yahoo.com>
2019-08-18 18:08:24 -0500

    changed

--- bin/run-tests-in-virtual-screen.sh
+++ bin/run-tests-in-virtual-screen.sh
@@ -8,7 +8,7 @@ then
         robot \
         --outputDir /app/reports \
         ${ROBOT_OPTIONS} \
-        --shm-size=1g /app/test
+        /app/test
 else
     xvfb-run \
         --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" \
